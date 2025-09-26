5e007c9143b4ca5706668a70143880fa0b87c097
Doris Lederle <dorislederle.01@gmail.com>
2025-09-26 21:52:07 +0200

    Fix code and add coverage step

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -20,6 +20,6 @@ func GetAPIKey(headers http.Header) (string, error) {
 		return "", errors.New("malformed authorization header")
 	}
 
-	//return splitAuth[1], nil
-	return "wrong-key", nil //temporarily broken code for testing
+	return splitAuth[1], nil
+	//return "wrong-key", nil //temporarily broken code for testing
 }
