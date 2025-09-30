40d804f356fbf124bdcd433e54463bfb412af460
Doris Lederle <dorislederle.01@gmail.com>
2025-09-30 11:59:25 +0200

    Fix gosec issues: add ReadHeaderTimeout and handle w.Write error

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -87,8 +88,9 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: 5 * time.Second, //gosec: schützt vor Slowlories
 	}
 
 	log.Printf("Serving on port: %s\n", port)
